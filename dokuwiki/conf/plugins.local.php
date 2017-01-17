4f3f5c50b35f1aace6a5521e3d7817a44be16197
Admin <rusidea.info@yandex.ru>
2017-01-17 23:28:41 +0300

    Полная версия v2.6.2 журнала "Аналитика"

--- dokuwiki/conf/plugins.local.php
+++ dokuwiki/conf/plugins.local.php
@@ -6,3 +6,4 @@
  * NOTE: Plugins will not be added to this file unless there is a need to override a default setting. Plugins are
  *       enabled by default.
  */
+$plugins['ckgedit'] = 0;
---
+++ dokuwiki/lib/plugins/ckgedit/lang/de/lang.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Leo Rudin <leo.rudin@gmx.ch>
+ * @author Padhie <develop@padhie.de>
+ * @author liz <marliza@web.de>
+ * @author Martin Sachers <martin.sachers@etron.at>
+ * @author ttthomas <tompommes@gmx.de>
+ */
+$lang['btn_dw_edit']           = 'DW Edit';
+$lang['dw_btn_fck_preview']    = 'CKG Vorschau';
+$lang['dw_btn_lang']           = 'Sprache';
+$lang['title_dw_delete']       = 'Seite l&ouml;schen';
+$lang['title_dw_edit']         = 'Arbeit speichern, schließen und zum ursprünglichen DokuWiki Editor wechseln';
+$lang['dw_btn_revert']         = 'R&uuml;ckg&auml;ngig';
+$lang['title_dw_revert']       = 'Zur&uuml;ck zur Backup-Version';
+$lang['title_dw_lang']         = 'Sprache f&uuml;r Rechtschreibkontrolle w&auml;hlen';
+$lang['title_dw_cancel']       = 'Editor schließen';
+$lang['btn_fck_edit']          = 'CKG Editor';
+$lang['confirm_delete']        = 'Sind Sie sicher, dass Sie diese Seite l&ouml;schen wollen?';
+$lang['confirm_preview']       = 'Jede nicht gespeicherte Bearbeitung wird verloren gehen.';
+$lang['editor_height']         = 'Editorhöhe';
+$lang['editor_height_title']   = 'Editor neuskalieren beim Neuladen';
+$lang['dw_btn_backup']         = 'Backup';
+$lang['title_dw_backup']       = 'Backup-Editor-Fenster & Renew-Zugangssperre';
+$lang['backup_empty']          = 'Es ist kein Backup vorhanden. Wollen Sie fortfahren?';
+$lang['btn_draft']             = 'Entwurf &ouml;ffnen';
+$lang['title_draft']           = 'Entwurf anzeigen und bearbeiten';
+$lang['btn_exit_draft']        = 'Entwurf schließen';
+$lang['title_exit_draft']      = 'Zurück zum aktuellen Dokument.';
+$lang['draft_msg']             = 'F&uuml;r dieses Dokument besteht eine Entwurfsversion. Der Knopf \'Entwurf\' schaltet zwischen diesem Dokument und dem Entwurf hin und her. Es k&ouml;nnen beide Dokumente bearbeitet oder gesichert werden.';
+$lang['whats_this']            = 'Was ist das?';
+$lang['complex_tables']        = 'Aktiviere komplexe Tabellen';
+$lang['minor_changes']         = 'Kleine Änderungen';
+$lang['discard_edits']         = 'Um Änderungen zu speichern klicke OK; um abzubrechen klicke Cancel.';
+$lang['dw_btn_styling']        = 'Schriftart bearbeiten';
+$lang['title_styling']         = 'Öffne mit Schriftart als markup';
+$lang['js']['font_err_throw']  = 'Zeichenformatierungs-Fehler';
+$lang['js']['dwp_save_err']    = 'Speichern nicht m&ouml;glich f&uuml;r:';
+$lang['js']['mediamgr_notice'] = 'Verwenden Sie den Link Dialog um Links einzufügen';
+$lang['uprofile_title']        = 'W&auml;hlen Sie ihren Standard Editor';
+$lang['btn_val_dw_fb']         = 'DW Dateibrowser';
+$lang['btn_val_ckg_fb']        = 'CKG Dateibrowser';
+$lang['btn_title_dw_fb']       = 'Speichern und Editor schließen und zum DW Dateibrowser wechseln';
+$lang['btn_title_ckg_fb']      = 'Speichern und Editor schließen und zum CKG Dateibrowser wechseln';
+$lang['formatdel']             = 'Markup wurde aus Header(n) entfernt: Von DokuWiki nicht unterst&uuml;tzt';
+$lang['fontdel']               = 'Markup wurde aus Link(s) entfernt: Von DokuWiki nicht unterst&uuml;tzt';
